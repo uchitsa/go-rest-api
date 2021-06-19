b21edbab1a6daa6d48ce5568a04298638f0bc54a
uchitsa <uchitsa@ya.ru>
2021-06-19 19:03:39 +0300

    update models

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gorilla/mux v1.6.2
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.3.0
+	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 )
--- models/account.go
+++ models/account.go
@@ -1,12 +1,16 @@
 package models
 
 import (
+	u "go-contacts/utils"
+	"os"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Token struct {
-	UserId uint
+	UserId uuid.uuid
 	jwt.StandardClaims
 }
 
@@ -18,11 +22,46 @@ type Account struct {
 }
 
 func (acc *Account) Create() map[string]interface{} {
+	if res, ok := acc.Validate(); !ok {
+		return res
+	}
 
+	passHash, _ := bcrypt.GenerateFromPassword([]byte(acc.Password), bcrypt.DefaultCost))
+	acc.Password := string(passHash)
+
+	 GetDB().Create(acc)
+
+	if acc.ID == "" {
+		return u.Message(false, "Failed to create account. Incorrect id")
+	}
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), &Token{UserId: acc.ID})
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	acc.Token = tokenString
+	acc.Password = ""
+
+	res := u.Message(true, "Account created")
+	res["account"] = acc
+	return res
 }
 
 func (acc *Account) Validate() (map[string]interface{}, bool) {
+	if len(acc.Login) < 5 {
+		return u.Message(false, "Password must be more than 5 symbols"), false
+	}
 
+	if len(acc.Password) < 6 {
+		return u.Message(false, "Password must be more than 6 symbols"), false
+	}
+
+	tmpAcc := &Account{}
+
+	err := GetDB().Table("accounts").Where("login = ?", acc.Login).First(tmpAcc).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return u.Message(false, "Login failed"), false
+	}
+
+	return u.Message(false, "Validate passed"), true
 }
 
 func GetUser(u uint) *Account {
